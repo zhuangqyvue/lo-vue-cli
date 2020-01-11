db2c0a3d3a3db39a3da121545ccf442619270bb2
zqy-vue <904047937@qq.com>
2020-01-11 19:16:40 +0800

    测试vue模板

--- generator-lo-vue-cli/generators/app/templates/src/assets/bus/eventBus.js
+++
@@ -1,5 +0,0 @@
-// 通用事件总线
-import Vue from 'vue'
-
-/* eslint-disable new-parens */
-export default new Vue
--- generator-lo-vue-cli/generators/app/templates/src/collect/index.js
+++
@@ -1,9 +0,0 @@
-import http from './axiosAdvanced'
-
-export default {
-  install: function (Vue, Option) {
-    Object.defineProperty(Vue.prototype, '$http', {
-      value: http
-    })
-  }
-}
